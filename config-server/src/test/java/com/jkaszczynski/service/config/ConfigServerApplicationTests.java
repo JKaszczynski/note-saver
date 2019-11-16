5342f559d376cab62816f9cbb08ec319e49b5afa
Jakub <j.kaszczynski@gmail.com>
2019-11-16 22:14:43 +0100

    Early projects setup

---
+++ config-server/src/test/java/com/jkaszczynski/service/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkaszczynski.service.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
