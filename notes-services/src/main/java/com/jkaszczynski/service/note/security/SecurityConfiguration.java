8a79cc1425a580dcd871895d4a6636faf8b1ada8
jkaszczynski <j.kaszczynski@gmail.com>
2020-01-08 21:38:30 +0100

    PrePost authorization enabled

--- notes-services/src/main/java/com/jkaszczynski/service/note/security/SecurityConfiguration.java
+++ notes-services/src/main/java/com/jkaszczynski/service/note/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.jkaszczynski.service.note.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Value("${spring.security.oauth2.client.provider.keycloak.jwk-set-uri}")
